b329e8a8295653db2c8cdcd9302fb9af32876161
ylc <756885686@qq.com>
2024-04-15 23:30:11 +0800

    优化LRU算法

--- utils/arrayUtils/LRUCache.js
+++ utils/arrayUtils/LRUCache.js
@@ -41,3 +41,10 @@ LRUCache.prototype.set = function (key,value) {
         this.map.delete(this.map.keys().next().value);
     }
 }
+/**
+ * 返回当前缓存中所存储的对象
+ * @returns {{[p: string]: any}}
+ */
+LRUCache.prototype.array = function () {
+    return Object.fromEntries(this.map);
+}
